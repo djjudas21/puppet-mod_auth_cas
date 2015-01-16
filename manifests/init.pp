15ee22f0a3ea131c130c9e3a7acbca31a407d8e5
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-01-16 16:16:18 +0000

    Add basic manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,15 +27,44 @@
 #    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
 #  }
 #
-# === Authors
-#
-# Author Name <author@domain.com>
-#
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Jonathan Gazeley, unless otherwise noted.
 #
-class mod_auth_cas {
+class mod_auth_cas (
+  $cacert,
+) {
 
+  package {'mod_auth_cas': ensure => installed}
 
+  file { 'auth_cas.conf':
+    name    => '/etc/httpd/conf.d/auth_cas.conf',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/httpd/mod_auth_cas/auth_cas.conf',
+    notify  => Service['httpd'],
+    require => Package['httpd'],
+    }
+
+  # Prepare the CAS cache directory
+  file { [ '/cas', '/cas/cache'] :
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'apache',
+    mode    => '0775',
+    seluser => 'system_u',
+    seltype => 'httpd_sys_content_t',
+    selrole => 'object_r',
+  }
+
+  # Don't create it, but do set security context
+  file { '/cas/cache/.metadata':
+    owner   => 'apache',
+    group   => 'apache',
+    mode    => '0600',
+    seluser => 'system_u',
+    seltype => 'httpd_sys_content_t',
+    selrole => 'object_r',
+  }
 }
